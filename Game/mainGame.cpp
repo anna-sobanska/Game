b344e6f787294af6bc27fa3e2f8c3aa4ca1dc173
Anna Sobanska <ania.webdev@gmail.com>
2021-06-17 15:36:31 +0200

    add a pseudorandom game

--- Game/mainGame.cpp
+++ Game/mainGame.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
 void guessingGame();
+void pseudoRandomNumbers();
 
 int main() {
 	std::cout << "Hello! Let's start the game" << std::endl;
-	guessingGame();
+	std::cout << "Select the game!" << std::endl;
+	std::cout << "1. Guessing game" << std::endl;
+	std::cout << "2. Random number guessing game" << std::endl;
+
+	int choice;
+	std::cin >> choice;
+
+	system("cls");
+
+	switch (choice)
+	{
+	case 1:
+		guessingGame();
+		break;
+	case 2:
+		pseudoRandomNumbers();
+		break;
+	default:
+		std::cout << "It is not valid!";
+		break;
+	}
 
 	system("pause>0");
 }
